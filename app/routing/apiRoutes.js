2dd7e5c53c2c8478eaeb8580934a4153c50ef969
James Dizon <dizonjerlin@gmail.com>
2019-04-04 00:02:12 -0700

    Pseduocoding done

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,4 @@
+// API ROUTES
+
+//TODO: Create GET routes for /api/friends (display a JSON of all possible friends)
+//TODO: Create POST routes for /api/friends (use to handle incoming survey results.)
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,4 @@
+// Routes 
+
+// TODO: Create Route for /survery (display survey page)
+// TODO: Create route for homepage -- should default to homepage
