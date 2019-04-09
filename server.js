e1a3b962c791ac1974b5cc0f2d1aa9888ce840c2
James Dizon <dizonjerlin@gmail.com>
2019-04-08 19:24:42 -0700

    updated files

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
+var apiRoutes = require("./app/routes/apiRoutes")(app)
+var htmlRoutes = require("./app/routes/htmlRoutes")(app)
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(htmlRoutes);
 app.use(apiRoutes);
 
-require("./app/routes/apiRoutes")(app)
-require("./app/routes/htmlRoutes")(app)
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
